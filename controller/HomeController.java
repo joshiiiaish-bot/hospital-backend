90e376cea7edf43336849be97f71395557a4f2d6
Aishwarya Joshi <joshiii.aish@gmail.com>
2026-05-27 15:10:48 +0530

    Connected frontend with backend

--- controller/HomeController.java
+++ controller/HomeController.java
@@ -1,13 +1,27 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Patient;
+import com.example.demo.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private PatientRepository patientRepository;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+
+        List<Patient> patients = patientRepository.findAll();
+
+        model.addAttribute("patients", patients);
+
         return "index";
     }
 }
